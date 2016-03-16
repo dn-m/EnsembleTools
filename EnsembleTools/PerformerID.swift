a04052aaffb928d5f7320409e96f7c65f964d567
jsbean <jbean46@gmail.com>
2016-03-16 17:28:33 -0400

    Integrate EnsembleTools

---
+++ EnsembleTools/PerformerID.swift
@@ -0,0 +1,11 @@
+//
+//  PerformerID.swift
+//  EnsembleTools
+//
+//  Created by James Bean on 3/16/16.
+//  Copyright © 2016 James Bean. All rights reserved.
+//
+
+import Foundation
+
+public typealias PerformerID = String
